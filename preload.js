874e638b57588717fdfe6f81c89c009e38b1f6ca
davicouto530 <coutodavi530@gmail.com>
2025-03-31 17:13:32 -0300

    Fazendo o pdf de cadastro da página cliente

--- preload.js
+++ preload.js
@@ -17,7 +17,8 @@ contextBridge.exposeInMainWorld('api', {
     placamotoWindow: () => ipcRenderer.send('placamoto-window'),
     osWindow: () => ipcRenderer.send('os-window'),
     dbStatus: (message) => ipcRenderer.on('db-status', message),
-    newClient: (client) => ipcRenderer.send('new-client', client)
+    newClient: (client) => ipcRenderer.send('new-client', client),
+    resetForm: (args) => ipcRenderer.on('reset-form', args)
 })
 
 function dbStatus(message) {
